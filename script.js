8c19905a257c0591d0104a440a76699510393dce
SammyOnikoyi <sammy.onikoyi@mail.utoronto.ca>
2025-03-31 10:10:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 async function fetchPPMData() {
     try {
         // Use the correct IP address of the ESP32
-        const response = await fetch("http://172.20.10.4/data"); // Use the ESP32 IP here
+        const response = await fetch("http://172.20.10.4/api/data"); // Use the ESP32 IP here
         const data = await response.json();
 
-        const ppmValue = data.ppm; // PPM value returned by ESP32
-        const esp32IP = data.ip;  // ESP32's IP address
+        const ppmValue = data.tdsValue; // PPM value returned by ESP32
         
         console.log("ESP32 IP Address:", esp32IP); // For testing, log the IP address
         updatePPMResult(ppmValue);
